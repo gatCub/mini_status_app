e112c03e7ed799c2df6d7cb00dac03051e77a726
gatCub <ruslanlatypov14@gmail.com>
2020-12-24 07:37:12 +0500

    v1.5

--- node_modules/@vkontakte/vkui/dist/components/PanelHeaderButton/PanelHeaderButton.d.ts
+++
@@ -1,8 +0,0 @@
-import { ButtonHTMLAttributes, FunctionComponent } from 'react';
-export interface PanelHeaderButtonProps extends ButtonHTMLAttributes<HTMLElement> {
-    primary?: boolean;
-    href?: string;
-    target?: string;
-}
-declare const PanelHeaderButton: FunctionComponent<PanelHeaderButtonProps>;
-export default PanelHeaderButton;
