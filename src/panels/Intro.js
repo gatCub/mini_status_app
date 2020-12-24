dc6c73e4de0181756eb6138f286af0d25fa01b75
gatCub <ruslanlatypov14@gmail.com>
2020-12-24 11:09:40 +0500

    v1.7

--- src/panels/Intro.js
+++ src/panels/Intro.js
@@ -1,11 +1,12 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
-import { platform, IOS } from '@vkontakte/vkui';
+import { platform, IOS, Button } from '@vkontakte/vkui';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
 import Group from '@vkontakte/vkui/dist/components/Group/Group';
 import Div from '@vkontakte/vkui/dist/components/Div/Div';
 import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
+import FixedLayout from '@vkontakte/vkui/dist/components/FixedLayout/FixedLayout';
 
 import './Intro.css';
 
@@ -25,8 +26,16 @@ const Intro = ({ id, snackbarError, fetchedUser, userHasSeenIntro, go }) => {
 							<Avatar src={fetchedUser.photo_200}/>}
 							<h2> Привет, {fetchedUser.first_name}!</h2>
 							<h3> Здесь ты можешь выбрать и установить себе статус</h3>
+							
 						</Div>
 					</Group>
+					<FixedLayout vertical='bottom'>
+							<Div>
+								<Button mode='commerce' size='xl' onClick={go}>
+									Вперед
+								</Button>
+							</Div>
+					</FixedLayout>
 				</Fragment>
 			}
 			{snackbarError}
